879775a1e5765049e3b67fd12e3d528db6d9ca7c
VOID <kouichi.hirachi@gmail.com>
2023-02-14 22:28:21 +0900

    some change

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link'
 export default function IndexPage() {
   return (
     <div>
-      Welcomr to UEC <Link href="/about">About</Link>
+      Welcomr to UEC! <Link href="/about">About</Link>
     </div>
   )
 }
