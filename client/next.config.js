5c942ac8c6bdf147c946c088d341e20cbccaa740
sys-fork_desktop <minato.sysfork@gmail.com>
2023-02-20 00:34:56 +0900

    Fixed config files

---
+++ client/next.config.js
@@ -0,0 +1,18 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    reactStrictMode: true,
+    compiler: (() => {
+        let compilerConfig = {
+            styledComponents: true,
+        }
+        if (process.env.NODE_ENV === 'production') {
+            compilerConfig = {
+                ...compilerConfig,
+                reactRemoveProperties: { properties: ['^data-testid$'] },
+            }
+        }
+        return compilerConfig
+    })(),
+}
+
+module.exports = nextConfig
