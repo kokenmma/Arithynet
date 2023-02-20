616d04ab3822a85a24fa4bf7b5e12130e5d41768
sys-fork_desktop <minato.sysfork@gmail.com>
2023-02-20 19:17:18 +0900

    Write Post components #3

--- client/components/organisms/Post.tsx
+++ client/components/organisms/Post.tsx
@@ -1,15 +1,68 @@
-interface PostProps {
+import { styled } from '@mui/material/styles';
+import Card from '@mui/material/Card';
+import CardHeader from '@mui/material/CardHeader';
+import CardMedia from '@mui/material/CardMedia';
+import CardContent from '@mui/material/CardContent';
+import CardActions from '@mui/material/CardActions';
+import Collapse from '@mui/material/Collapse';
+import Avatar from '@mui/material/Avatar';
+import IconButton, { IconButtonProps } from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import { red } from '@mui/material/colors';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import ShareIcon from '@mui/icons-material/Share';
+import ReplyIcon from '@mui/icons-material/Reply';
+import RepeatIcon from '@mui/icons-material/Repeat';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import MoreVertIcon from '@mui/icons-material/MoreVert';
+import Details from '../atoms/Details'
+
+export interface PostProps {
     photoURL: string,
     userName: string,
     userId: string,
-    postHTML: string,
+    postHTML: JSX.Element,
+    postTime: string,
     replyCount: number,
     repostCount: number,
-    favCount: number
+    favCount: number,
+    isFavedByU: boolean,
+    isRepostedByU: boolean
 }
 
-const Post = (Props: PostProps) => {
-    return <></>
+const Post = (props: PostProps) => {
+    return (
+        <Card sx={{ maxWidth: 600 }}>
+            <CardHeader
+                avatar={
+                    <Avatar src={props.photoURL} aria-label="icon" />
+                }
+                action={
+                    <Details {...props}/>
+                }
+                title={props.userName + '@' + props.userId}
+                subheader={props.postTime}
+            />
+            <CardContent>{props.postHTML}</CardContent>
+            <CardActions disableSpacing>
+                <IconButton aria-label="reply">
+                    <ReplyIcon />
+                </IconButton>
+                <Typography variant="body2">{props.replyCount}</Typography>
+                <IconButton aria-label="repost">
+                    <RepeatIcon />
+                </IconButton>
+                <Typography variant="body2">{props.repostCount}</Typography>
+                <IconButton aria-label="favorite">
+                    <FavoriteIcon />
+                </IconButton>
+                <Typography variant="body2">{props.favCount}</Typography>
+                <IconButton aria-label="share">
+                    <ShareIcon />
+                </IconButton>
+            </CardActions>
+        </Card>
+    );
 }
 
 export default Post
--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,9 +1,11 @@
-import Link from 'next/link'
+import Post from '../components/organisms/Post'
 
 export default function IndexPage() {
-  return (
-    <div>
-      Welcomr to UEC! <Link href="/about">About</Link>
-    </div>
-  )
+    return (
+        <Post
+            photoURL='' userName="testuser" userId='123456abc' postHTML={<span>Welcome to UEC!!</span>}
+            replyCount={1} repostCount={2} favCount={3} postTime="February 20, 2023"
+            isFavedByU={true} isRepostedByU={true}
+        />
+    )
 }
