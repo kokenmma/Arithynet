eee72c792919193d8dd28cc6beb948380826ceb2
sys-fork_desktop <minato.sysfork@gmail.com>
2023-02-21 01:21:47 +0900

    Write CreatingPost component

--- client/components/CreatingPost.tsx
+++ client/components/CreatingPost.tsx
@@ -1,9 +1,48 @@
-interface CreatingPostProps {
+import { getCookie } from 'cookies-next';
+import Box from '@mui/material/Box';
+import Card from '@mui/material/Card';
+import CardHeader from '@mui/material/CardHeader';
+import CardContent from '@mui/material/CardContent';
+import CardActions from '@mui/material/CardActions';
+import AddPhotoAlternateOutlinedIcon from '@mui/icons-material/AddPhotoAlternateOutlined';
+import PollOutlinedIcon from '@mui/icons-material/PollOutlined';
+import Avatar from '@mui/material/Avatar';
+import IconButton from '@mui/material/IconButton';
 
-}
+const style = {
+  position: 'absolute' as 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 400,
+  bgcolor: 'background.paper',
+  border: '2px solid #000',
+  boxShadow: 24,
+  p: 4,
+};
 
-const CreatingPost = (props: CreatingPostProps) => {
-    return <></>
-}
+const CreatingPost = () => {
+  return (
+    <Box sx={style}>
+      <Card sx={{ maxWidth: 900 }}>
+        <CardHeader
+          avatar={<Avatar src={getCookie('photoURL') as string} aria-label='icon' />}
+          title={getCookie('userName') + '@' + getCookie('userId')}
+        />
+        <CardContent>
+          <textarea></textarea>
+        </CardContent>
+        <CardActions disableSpacing>
+          <IconButton aria-label='add image'>
+            <AddPhotoAlternateOutlinedIcon />
+          </IconButton>
+          <IconButton aria-label='add poll'>
+            <PollOutlinedIcon />
+          </IconButton>
+        </CardActions>
+      </Card>
+    </Box>
+  );
+};
 
-export default CreatingPost
+export default CreatingPost;
