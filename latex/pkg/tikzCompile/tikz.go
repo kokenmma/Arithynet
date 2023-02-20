195728bd46009a296361756b45102413ccb4b7fe
kienn-HCl <87010782+kienn-HCl@users.noreply.github.com>
2023-02-20 13:59:51 +0900

    docker: debian -> alpine

--- latex/pkg/tikzCompile/tikz.go
+++ latex/pkg/tikzCompile/tikz.go
@@ -1,51 +1,51 @@
 package tikz
 
 import (
+	"io/ioutil"
 	"log"
 	"os/exec"
-    "io/ioutil"
 )
 
 func Compile(txt string) {
-    err := ioutil.WriteFile("./src/index.tex", []byte(txt), 0644)
-    if err != nil {
-        log.Fatal(err)
-    }
+	err := ioutil.WriteFile("./src/index.tex", []byte(txt), 0644)
+	if err != nil {
+		log.Fatal(err)
+	}
 
-    cmd := exec.Command("./tectonic", "--appimage-extract-and-run", "-X", "build")
+	cmd := exec.Command("tectonic", "-X", "build")
 	stdoutStderr, err := cmd.CombinedOutput()
 	if err != nil {
-	    log.Printf("%s\n", stdoutStderr)
+		log.Printf("%s\n", stdoutStderr)
 		log.Fatal(err)
 	}
 }
 
 func pdf2svg() {
-    cmd := exec.Command("pdftocairo", "-svg", "./build/default/default.pdf")
+	cmd := exec.Command("pdftocairo", "-svg", "./build/default/default.pdf")
 	stdoutStderr, err := cmd.CombinedOutput()
 	if err != nil {
-	    log.Printf("%s\n", stdoutStderr)
+		log.Printf("%s\n", stdoutStderr)
 		log.Fatal(err)
 	}
 }
 
 func xdv2svg() {
-    cmd := exec.Command("dvisvgm", "texput.dvi")
+	cmd := exec.Command("dvisvgm", "texput.dvi")
 	stdoutStderr, err := cmd.CombinedOutput()
 	if err != nil {
-	    log.Printf("%s\n", stdoutStderr)
+		log.Printf("%s\n", stdoutStderr)
 		log.Fatal(err)
 	}
 }
 
 func TikzWrapper(txt string) string {
-    Compile(txt)
-    pdf2svg()
-    //xdv2svg()
+	Compile(txt)
+	pdf2svg()
+	//xdv2svg()
 
-    f,err := ioutil.ReadFile("default.svg")
-    if err != nil {
-        log.Fatal(err)
-    }
-    return string(f)
+	f, err := ioutil.ReadFile("default.svg")
+	if err != nil {
+		log.Fatal(err)
+	}
+	return string(f)
 }
