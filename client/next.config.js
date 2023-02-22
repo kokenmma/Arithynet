b62cec90c270d3d45e5983232d5a19e7a6821d3e
VOID <kouichi.hirachi@gmail.com>
2023-02-23 02:56:45 +0900

    add env files

--- client/next.config.js
+++ client/next.config.js
@@ -14,15 +14,7 @@ const nextConfig = {
             }
         }
         return compilerConfig
-    })(),
-    env: {
-        apiKey: process.env.apiKey,
-        authDomain: process.env.authDomain,
-        projectId: process.env.projectId,
-        storageBucket: process.env.storageBucket,
-        messagingSenderId: process.env.messagingSenderId,
-        appId: process.env.appId,
-    }
+    })()
 }
 
 module.exports = nextConfig
--- client/services/firebase.ts
+++ client/services/firebase.ts
@@ -2,13 +2,16 @@ import { getFirestore } from 'firebase/firestore';
 import { initializeApp } from 'firebase/app';
 
 const firebaseConfig = {
-  apiKey: process.env.apiKey,
-  authDomain: process.env.authDomain,
-  projectId: process.env.projectId,
-  storageBucket: process.env.storageBucket,
-  messagingSenderId: process.env.messagingSenderId,
-  appId: process.env.appId
+  apiKey: process.env.NEXT_PUBLIC_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
+  databaseURL: process.env.NEXT_PUBLIC_DATABASE_URL,
+  projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_APP_ID
 };
+
+console.log(firebaseConfig)
 
 export const app = initializeApp(firebaseConfig);
 
