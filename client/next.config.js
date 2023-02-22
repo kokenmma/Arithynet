fe52bba7c4048c91dc5e25cd307194341456cbf1
VOID <kouichi.hirachi@gmail.com>
2023-02-23 02:13:02 +0900

    update env

--- client/next.config.js
+++ client/next.config.js
@@ -8,11 +8,21 @@ const nextConfig = {
         if (process.env.NODE_ENV === 'production') {
             compilerConfig = {
                 ...compilerConfig,
-                reactRemoveProperties: { properties: ['^data-testid$'] },
+                reactRemoveProperties: {
+                    properties: ['^data-testid$']
+                },
             }
         }
         return compilerConfig
     })(),
+    env: {
+        apiKey: process.env.apiKey,
+        authDomain: process.env.authDomain,
+        projectId: process.env.projectId,
+        storageBucket: process.env.storageBucket,
+        messagingSenderId: process.env.messagingSenderId,
+        appId: process.env.appId,
+    }
 }
 
 module.exports = nextConfig
--- client/services/firebase.ts
+++ client/services/firebase.ts
@@ -2,13 +2,12 @@ import { getFirestore } from 'firebase/firestore';
 import { initializeApp } from 'firebase/app';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBOk3fPTI6ExbFZjAr0hQV0CDhuzdLIPSI",
-  authDomain: "arithynet.firebaseapp.com",
-  databaseURL: "https://arithynet-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "arithynet",
-  storageBucket: "arithynet.appspot.com",
-  messagingSenderId: "618699717752",
-  appId: "1:618699717752:web:2bbaf77fe56ff09322c18d"
+  apiKey: process.env.apiKey,
+  authDomain: process.env.authDomain,
+  projectId: process.env.projectId,
+  storageBucket: process.env.storageBucket,
+  messagingSenderId: process.env.messagingSenderId,
+  appId: process.env.appId
 };
 
 export const app = initializeApp(firebaseConfig);
