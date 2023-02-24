963e5fe83b0e50c9ee43c2650922d5693df18bcf
VOID <kouichi.hirachi@gmail.com>
2023-02-25 01:13:03 +0900

    add tikz render

--- client/components/RenderContent.tsx
+++ client/components/RenderContent.tsx
@@ -18,9 +18,21 @@ interface RenderContentProps {
 }
 
 const RenderContent = ({ content, images = [] }: RenderContentProps) => {
+  const [replaced, setReplaced] = useState<string>('');
+  useEffect(() => {
+    // replace tikz code block to image
+    console.log(content);
+    const regex = /\\begin{tikzpicture}(.*?)\\end{tikzpicture}/ms;
+    let index = 0;
+    while (content.match(regex) !== null) {
+      const replaceContent = images.length > index ? `![SVG](${images[index]})` : '';
+      content = content.replace(regex, replaceContent);
+    }
+    setReplaced(content);
+  }, [content]);
   return (
     <ReactMarkdown remarkPlugins={[remarkMath]} rehypePlugins={[rehypeKatex]}>
-      {content}
+      {replaced}
     </ReactMarkdown>
   );
 };
